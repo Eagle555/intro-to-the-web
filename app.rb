f9c0130fb7b45e0d9e6162d498460954742952ec
Tim Turvey <tturvey@hotmail.co.uk>
2021-10-04 15:45:18 +0100

    red dashed border

--- app.rb
+++ app.rb
@@ -11,5 +11,7 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<img src='https://i.imgur.com/jFaSxym.png'>"
+  "<div style='border: 3px dashed red'>
+  <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
 end
